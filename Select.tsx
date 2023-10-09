60eb557c17860da513322a926d60276c66fa7a78
xalivo <87896066+xalivo@users.noreply.github.com>
2023-10-09 21:55:55 +0200

    added onChange & setSelectedItem functionality

--- Select.tsx
+++ Select.tsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 interface ISelect<T> {
     items: T[];
     optionTitle: string;
     optionValue: string;
-    onSelect?: () => void;
-    onChange?: () => void;
-    
+    setSelectedItem?: (item: T) => void;
+    onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    bg?: string;
 }
 
-export function Select<T>({items, optionTitle, optionValue, onSelect, onChange}: ISelect<T>) {
-  return (
-    <select onChange={onChange}>
-        {items.map(x => <option value={x[optionValue]}>{x[optionTitle]}</option>)}
-    </select>
-  );
+interface IIndexable<T> {
+    [key: string]: T;
+}
+
+export function Select<T>({items, optionTitle, optionValue, setSelectedItem, onChange, bg}: ISelect<T>) {
+    const doesPropertyExist = (property: string, object: T): boolean => {
+        return Object.prototype.hasOwnProperty.call(object, property);
+    }
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        if (onChange !== undefined) {
+            onChange(e);
+        } else if (setSelectedItem !== undefined) {
+            setSelectedItem(items.find(x => e.currentTarget.value === ("" + (x as IIndexable<T>)[optionValue]))!);
+        }
+    }
+
+    return (
+        <select style={{backgroundColor: bg}} onChange={handleOnChange}>
+            {items.map((x, index) => {
+                if (!doesPropertyExist(optionValue, x)) {
+                    throw new Error(`better-react-select: Property ${optionValue} does not exist on item.`);
+                }
+                return (<option key={index} value={"" + (x as IIndexable<T>)[optionValue]}>{"" + (x as IIndexable<T>)[optionTitle]}</option>)
+            })}
+        </select>
+    );
 }
